1f3828119491cb6bf1c6c5749a3adae946aa65b2
LiuBo <1028149516@qq.com>
2024-09-26 18:17:56 +0800

    update

--- js/request.js
+++ js/request.js
@@ -1,4 +1,5 @@
 export const login=async (value)=>{
- const result=await fetch('./js/files/login.json').then((res)=>res.json())
-  console.log(result);
+ const result=await fetch('./js/files/login.json',{method:'POST',myvalue:value}).then((res)=>res.json())
+  //console.log(result,value);
+  return result;
 }
